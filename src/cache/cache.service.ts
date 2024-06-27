01493b474db74ea45104a5684a3e5b8439af0e9c
Trevizan-so <guilherme.trevizan.so@gmail.com>
2024-06-26 23:19:54 -0300

    Fix: Ajuste teste CacheService

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -1,28 +1,27 @@
-import { Injectable, UnauthorizedException, Inject, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { ICacheService } from './cache.interface';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
+import { Injectable, UnauthorizedException, Inject, Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ICacheService } from "./cache.interface";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
 
 @Injectable()
-export class CacheService implements ICacheService{
-    
-    constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache, private configService: ConfigService) {}
+export class CacheService implements ICacheService {
+    constructor(
+        @Inject(CACHE_MANAGER) private cacheManager: Cache,
+        private configService: ConfigService
+    ) {}
 
     private readonly logger = new Logger(CacheService.name);
 
-    
-
-    async findKey(key:string):Promise<any>{
+    async findKey(key: string): Promise<any> {
         return await this.cacheManager.get(key);
-    };
-    async setKey(key:string, value:any):Promise<void>{
-        const defaultTtl = this.configService.get<number>("CACHE_TTL",10000);
+    }
+    async setKey(key: string, value: any): Promise<void> {
+        const defaultTtl = this.configService.get<number>("CACHE_TTL", 10000);
         this.logger.log(`Setting ${key} with value "${value}" to cache`);
         await this.cacheManager.set(key, value, defaultTtl);
-    };
-    async setKeyWithTTL(key:string,value:any,ttl:number):Promise<void>{
+    }
+    async setKeyWithTTL(key: string, value: any, ttl: number): Promise<void> {
         await this.cacheManager.set(key, value, ttl);
-    };
-
+    }
 }
--- test/cache/cache.service.spec.ts
+++ test/cache/cache.service.spec.ts
@@ -1,13 +1,24 @@
 import request from "supertest";
 import { Test } from "@nestjs/testing";
-
+import { Cache } from "cache-manager";
+import { TestingModule } from "@nestjs/testing";
+import { AppModule } from "../../src/app.module";
 import { CacheService } from "../../src/cache/cache.service";
+import { INestApplication } from "@nestjs/common";
 
 describe("CacheService", () => {
     let cacheService: CacheService;
+    let app: INestApplication;
 
-    beforeEach(() => {
-        cacheService = new CacheService();
+    beforeEach(async () => {
+        const moduleRef: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+          }).compile();
+          app = moduleRef.createNestApplication();
+          await app.init();
+          cacheService = moduleRef.get(CacheService);
+
+        
     });
     it("shoud retrive data using key", async () => {
         let key = "jwt:token";
